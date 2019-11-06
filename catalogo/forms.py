367bb30ad61d2faa1d320aa9ade01cd1f99f10ac
Nico0411 <Nicolaspalmaprats1@gmail.com>
2019-11-05 23:21:26 -0300

    Actualizacion.Se arregla el css de registro

--- catalogo/forms.py
+++ catalogo/forms.py
@@ -4,8 +4,10 @@ from django.contrib.auth.models import User
 from .models import Profile
 
 class registerForm(UserCreationForm):
-	name = forms.CharField(max_length=30, required=True)
-	email = forms.EmailField(required=True)
+	name = forms.CharField(max_length=30, required=True, widget=forms.TextInput(attrs={'class': 'form-control','id': 'inputName' }))
+	email = forms.EmailField(required=True, widget=forms.EmailInput(attrs={'class': 'form-control', 'id': 'inputEmail'}))
+	password1 = forms.CharField(widget= forms.PasswordInput(attrs={'class': 'form-control', 'id': 'inputPassword'}))
+	password2 = forms.CharField(widget= forms.PasswordInput(attrs={'class': 'form-control', 'id': 'inputConfirmPassword'}))
 
 	class Meta:
 		model = User
@@ -16,5 +18,8 @@ class registerForm(UserCreationForm):
 			'password1',
 			'password2',
 		)
+		widgets = {
+			'username': forms.TextInput(attrs={'class': 'form-control','id':'inputUserName'}),
+		}
 
 		
